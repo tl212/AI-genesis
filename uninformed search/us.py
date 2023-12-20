a2de556cdece5958d70b8c751d4acd11c536b692
TL33 <tl33@seas.upenn.edu>
2023-12-20 03:37:33 -0500

    change all functions name for readbility

--- uninformed search/us.py
+++ uninformed search/us.py
@@ -5,32 +5,30 @@ from collections import deque
 # N-Queens
 
 # implements all possible placements in n x n board
-def all_num_placement(n):
-    return math.factorial(n*n) // (math.factorial(n) * math.factorial(n * n-n))
+def num_placements_all(n):
+    return math.factorial(n*n) // (math.factorial(n) * math.factorial(n*n -n))
 
-# calculates the number of possible placements of in the board such that each 
-# row contains only one queens 
-def one_per_row_num_placement(n):
+def num_placements_one_per_row(n):
     return n ** n
 
-def valid_queens_n(board):
+def n_queens_valid(board):
     n = len(board)
     for i in range(n):
-        # comparing with Qs that comes after
-        for j in range(i + 1, n):
-            # checking diagonally
+        for j in range(i + 1, n): #comparing with the Qs that comes after
+            #checking diagonally
             if board[i] == board[j] or abs(board[i] - board[j]) == j - i:
                 return False
     return True
 
-def solutions_solutions_n(n):
-    def helper_n_queens(n, board):
-        if len(board) == n:
+
+def n_queens_solutions(n):
+    def n_queens_helper(n, board):
+        if len(board) == n: #if board is complete
             yield board
             return
         
-        for col in range(n):
-            if valid_queens_n(board + [col]):
-                yield from helper_n_queens(n, board + [col])
-    
-    return list(helper_n_queens(n, []))
+        for col in range(n): #place queen in current column and recursevily
+            if n_queens_valid(board + [col]):
+                yield from n_queens_helper(n, board + [col])
+                
+    return list(n_queens_helper(n, []))
