5fe8f6701db90493a5ff77f7ff22850b4a99a14b
TL33 <tl33@seas.upenn.edu>
2023-12-24 14:36:02 -0500

    defined tile puzzle board

--- informed search/is.py
+++ informed search/is.py
@@ -5,3 +5,9 @@ from queue import PriorityQueue
 from math import sqrt
 from heapq import heappush, heappop
 
+# tile puzzle
+def create_tile_puzzle(rows, cols):
+    board = [[i * cols + j +1 for j in range(cols)] for i in range(cols)]
+    board[-1][-1] = 0
+    return TilePuzzle(board)
+
